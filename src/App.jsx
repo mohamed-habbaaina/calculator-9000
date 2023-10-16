db751f7af904436178f90f4ca44ae73bdd5783d1
mohamed-habbaaina <mohamed.habbaaina@laplateforme.io>
2023-10-16 11:37:26 +0200

    Add Titel component

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+
+import './App.css'
+import Title from './component/Title'
+
+function App() {
+
+  return (
+    <>
+      <div>
+        <Title text="Calculator 9000" />
+      </div>
+    </>
+  )
+}
+
+export default App
