bc612e0e8a3cd7bdc21cae438a68d05817bb41e7
mohamed-habbaaina <mohamed.habbaaina@laplateforme.io>
2023-10-17 09:26:48 +0200

    Add components

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,18 @@
 
 import './App.css'
+import Calculator from './component/Calculator'
 import Title from './component/Title'
 
-function App() {
+export default function App() {
 
   return (
     <>
       <div>
         <Title text="Calculator 9000" />
+
+        <Calculator />
+
       </div>
     </>
   )
 }
-
-export default App
